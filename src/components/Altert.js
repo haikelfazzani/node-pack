0d0eaa81ec8fa7a905bd1e8721cc3916b721000d
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-07-22 15:19:05 +0100

    update app

--- src/components/Altert.js
+++ src/components/Altert.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Altert({ dataLength }) {
   return (
@@ -6,7 +7,9 @@ export default function Altert({ dataLength }) {
       {dataLength < 1 &&
         (<div className="alert alert-primary alert-nfound" role="alert">
           <h4>Nothing found!</h4>
-          <p>No libraries were found. You can Submit one..</p>
+          <p>No libraries were found. You can 
+          <Link to='/add-package' className="btn-link ml-1">Submit</Link> one..
+          </p>
         </div>)
       }
     </>
