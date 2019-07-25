5b314b8676531d2221fb0ad81669d57b8ebac38d
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-07-25 03:44:36 +0100

    update app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="mt-5">
-      <p className="font-s14">CREATED AND MANTAINED BY HAIKEL FAZZANI</p>
+      <p className="text-muted font-s14">CREATED AND MANTAINED BY HAIKEL FAZZANI</p>
     </footer>
   )
 }
