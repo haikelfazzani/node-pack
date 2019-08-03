050308d16467c8e096c3cf424fdfbdcba4aacb05
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-08-03 12:38:18 +0100

    update app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,28 @@ const githubLink = "https://github.com/haikelfazzani/node-pack/issues";
 
 export default function Footer() {
   return (
-    <footer className="w-100 d-md-flex flex-sm-row justify-content-md-between p-w pt-0 mt-5">
-      <p className="text-muted font-s14">
-        CREATED AND MAINTAINED BY <ExternalLink
-          link="https://github.com/haikelfazzani"
-          text="HAIKEL FAZZANI"
-        />
-      </p>
-      <p className="text-muted font-s14">
-        © 2019 NODEPACK - <ExternalLink link={githubLink} text="issues" />
-      </p>
+    <footer className="w-100 p-w pt-0 mt-5">
+
+      <div className="row mt-4">
+
+        <div className="col-md-4">
+          <p className="text-muted font-s14">
+            CREATED AND MAINTAINED BY <ExternalLink
+              link="https://github.com/haikelfazzani"
+              text="HAIKEL FAZZANI" />
+          </p>
+        </div>
+
+        <div className="col-md-4"></div>
+
+        <div className="col-md-4">
+          <p className="text-muted font-s14 float-md-right">
+            © 2019 NODEPACK - <ExternalLink link={githubLink} text="issues" />
+          </p>
+        </div>
+
+      </div>
+
     </footer>
   )
 }
