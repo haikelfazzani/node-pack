6b1826e773e5d12a0fc88e2e66de21402da7ebc2
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-08-05 18:06:53 +0100

    add scan page

--- src/containers/PackageJson.js
+++ src/containers/PackageJson.js
@@ -8,6 +8,9 @@ export default class PackageJson extends React.Component { // data.dependencies
     for (let d in this.props.data.dependencies) {
       dep.push({ p: d, v: this.props.data.dependencies[d] })
     }
+
+    
+
     return (
       <ul className="list-group w-50 mx-auto">
         {dep.map((d, i) => <li key={i} className="list-group-item">{d.p} - {d.v}</li>)}
